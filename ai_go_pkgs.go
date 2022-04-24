d1312a2334d55557b261aa5b44c1386a8122dda0
AntonyIS <antony@ariyafinergy.com>
2022-04-24 10:39:12 +0300

    Adding GoEngineer struct and methods

--- ai_go_pkgs.go
+++ ai_go_pkgs.go
@@ -2,6 +2,33 @@ package ai_go_pkgs
 
 import "fmt"
 
+type GoEngineer struct {
+	name, company, country string
+}
+
 func Sample() {
 	fmt.Println("Hello world")
+}
+
+func (ge GoEngineer) PrintDetails() {
+	fmt.Printf("My name is %v, developer at %v , from %v", ge.name, ge.company, ge.country)
+	fmt.Println()
+}
+
+func PrintGoEnginners(ge []GoEngineer) {
+	goEngineers := []GoEngineer{
+		{
+			name:    "Antony Injila",
+			company: "Ariya Finergy",
+			country: "Kenya",
+		},
+		{
+			name:    "Mohameed Mwiji",
+			company: "Lori Systems",
+			country: "Kenya",
+		},
+	}
+	for _, engineer := range goEngineers {
+		engineer.PrintDetails()
+	}
 }
